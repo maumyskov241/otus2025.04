45324c015b3edd097cdf7513f729a7ff4d97b673
maumyskov <maumyskov@rt-dc.ru>
2025-05-08 18:52:52 +0300

    Feat: testdrive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,38 +6,34 @@ defineProps({
 })
 
 const count = ref(0)
+const href = ref('https://vuejs.org/guide/quick-start.html#local')
+const target = ref('_blank')
+const count2 = ref(9000)
+const color = ref('green')
+const func = (x) => {alert(x)}
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    
+    <button v-if="count % 2 == 0" type="button" @mouseover="count2--;">count : {{ count2 }}</button>
+    <button v-else @mouseover="func('test')" type="button">count : 0000</button>
+    <button type="button" :class="color" @click="count++">count is {{ count }}</button>
+    <button v-show="count % 5 == 0" type="button" v-on:click="count2--;count++;count2--;count++;"> count + count2</button>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p><a :href="href" :target="target">{{target}}</a></p>
+
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.green {
+  background-color: green;
 }
 </style>
